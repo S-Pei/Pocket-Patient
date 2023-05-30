8faeaa40889637381177c81280067e12609ce616
huanswenham <huanswenham@gmail.com>
2023-05-30 20:52:24 +0100

    Comment initial test

--- patientOnCall/tests.py
+++ patientOnCall/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-class InitialTest(TestCase):
-    def test_setup(self):
-        """Test case to initialise the test suites"""
-        self.assertEqual(2, 1)                            
+# class InitialTest(TestCase):
+#     def test_setup(self):
+#         """Test case to initialise the test suites"""
+#         self.assertEqual(1, 1)                            
