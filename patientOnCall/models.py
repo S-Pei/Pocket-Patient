4cb64eeb569f405a302cdd05b78b6e8ebb0d6408
huanswenham <huanswenham@gmail.com>
2023-05-30 14:27:16 +0100

    Migrate patient db

--- patientOnCall/models.py
+++ patientOnCall/models.py
@@ -1,3 +1,10 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  firstname = models.CharField(max_length=500)
+  lastname = models.CharField(max_length=500)
+
