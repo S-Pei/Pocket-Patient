66f3a103232c30da3a49e1c51ef53dcda8afce04
Patcharapol Tangchalermkul <pt321@aube.doc.ic.ac.uk>
2023-05-30 15:47:24 +0100

    test with change in html

--- patientOnCall/views.py
+++ patientOnCall/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse("Hello GROUP39!")
 
 def displayDoctor(request):
     return HttpResponse("DOCTOR ON CALLLLLLLLL!")
