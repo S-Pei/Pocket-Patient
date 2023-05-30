9ce5f1af296ec9492b58086e038cd2840b433e53
Patcharapol Tangchalermkul <pt321@aube.doc.ic.ac.uk>
2023-05-30 16:01:32 +0100

    Test CI to fail and not deploy

--- patientOnCall/tests.py
+++ patientOnCall/tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 class InitialTest(TestCase):
     def test_setup(self):
         """Test case to initialise the test suites"""
-        self.assertEqual(1, 1)                            
+        self.assertEqual(2, 1)                            
--- patientOnCall/views.py
+++ patientOnCall/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello GROUP39!")
+    return HttpResponse("SHOULD NOT SHOW THIS Hello GROUP39!")
 
 def displayDoctor(request):
     return HttpResponse("DOCTOR ON CALLLLLLLLL!")
