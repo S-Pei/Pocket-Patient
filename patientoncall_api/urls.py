01de6c97abb4f49b27b50b1f216ff34cf0be53a6
huanswenham <huanswenham@gmail.com>
2023-05-31 18:57:26 +0100

    Initial Implemention jwt token for authentication

--- patientoncall_api/urls.py
+++ patientoncall_api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views
+from .views import PatientApiView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('patient-data/<int:patientId>', PatientApiView.as_view()),
     # path('pageDoctor/', views.displayDoctor, name='index'),
     # path('api/getpatient', views.getPatient, name='apiGetPatient')
 ]
--- patientoncall_api/views.py
+++ patientoncall_api/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("API ON CALLLLLLLLL!")
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+
+from .models import Patient
+from .serializers import PatientSerializer
+
+
+class PatientApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, patientId, *args, **kwargs):
+        '''
+        List all data for given requested patient user
+        '''
+        patient = Patient.objects.get(patientId = patientId)
+        serializer = PatientSerializer(patient, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
