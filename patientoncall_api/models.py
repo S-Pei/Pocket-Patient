d573225e644051249596d5b16d5acf881e99c5e0
huanswenham <huanswenham@gmail.com>
2023-05-31 21:04:09 +0100

    Create LabHistory and MedicalHistory models

--- patientoncall_api/models.py
+++ patientoncall_api/models.py
@@ -1,9 +1,16 @@
 import uuid
 
 from django.db import models
+from django.contrib.auth.models import User
 
-class Patient(models.Model):
+class MedicalHistory(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-  patientId = models.IntegerField(editable=True, default=-1)
-  firstname = models.CharField(max_length=500)
-  lastname = models.CharField(max_length=500)
+  patient = models.ForeignKey(User, on_delete=models.CASCADE)
+  date = models.DateField()
+  summary = models.TextField(max_length=2048)
+
+class LabHistory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    report = models.ImageField(upload_to="labreportimages/")
--- patientoncall_api/views.py
+++ patientoncall_api/views.py
@@ -5,18 +5,16 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 
-from .models import Patient
-from .serializers import PatientSerializer
-
 
 class PatientApiView(APIView):
     # add permission to check if user is authenticated
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request, patientId, *args, **kwargs):
-        '''
-        List all data for given requested patient user
-        '''
-        patient = Patient.objects.get(patientId = patientId)
-        serializer = PatientSerializer(patient, many=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    # def get(self, request, patientId, *args, **kwargs):
+    #     '''
+    #     List all data for given requested patient user
+    #     '''
+    #     patient = request.user
+    #     print(patient.username)
+    #     serializer = PatientSerializer(patient, many=False)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
