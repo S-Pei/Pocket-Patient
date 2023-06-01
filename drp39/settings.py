52ed2647e21d327a9ec48ccd56bb127c10ed29d6
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-01 15:21:14 +0100

    Rename static files in settings

--- drp39/settings.py
+++ drp39/settings.py
@@ -37,7 +37,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 
---
+++ staticfiles/patientOnCall/index.js
@@ -0,0 +1,25 @@
+document.getElementById("patient-search-submit").addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  let patientId = document.getElementById("patient-id").value;
+  let patientName = document.getElementById("patient-name").value;
+
+  console.log(patientId)
+  console.log(patientName)
+  //compare to database
+  $.ajax({
+    type: "POST",
+    url: "api/doctor/patient-data/",
+    data: {
+        'patientId': patientId,
+        'patientName': patientName
+    },
+    success: function (returned_value) {
+      if (returned_value.ok == true) { 
+        window.location.href = "main/"
+        console.log(returned_value);
+      }
+    },
+    error: function () { }
+  });
+})
