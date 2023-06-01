59f7fbaf09f15e30cc674426b9b44cf8ef41cec5
Mitosis <lostwanderer@Mitosis>
2023-06-01 15:17:23 +0100

    use session storage to store data

--- patientOnCall/static/patientOnCall/index.js
+++ patientOnCall/static/patientOnCall/index.js
@@ -16,8 +16,10 @@ document.getElementById("patient-search-submit").addEventListener("click", (e) =
     },
     success: function (returned_value) {
       if (returned_value.ok == true) { 
+        console.log(returned_value["lab-history"])
+        sessionStorage.setItem("labHistory", JSON.stringify(returned_value["lab-history"]))
+        sessionStorage.setItem("medicalHistory", JSON.stringify(returned_value["medical-history"]))
         window.location.href = "main/"
-        console.log(returned_value);
       }
     },
     error: function () { }
---
+++ patientOnCall/static/patientOnCall/info.js
@@ -0,0 +1,26 @@
+document.getElementById("name").addEventListener("mouseover", (e) => {
+
+    const labHistory = JSON.parse(sessionStorage.getItem("labHistory"))
+    const medicalHistory = JSON.parse(sessionStorage.getItem("medicalHistory"))
+  
+    console.log(labHistory[0])
+    console.log(medicalHistory)
+    //compare to database
+    // $.ajax({
+    //   type: "POST",
+    //   url: "api/doctor/patient-data/",
+    //   data: {
+    //       'patientId': patientId,
+    //       'patientName': patientName
+    //   },
+    //   success: function (returned_value) {
+    //     if (returned_value.ok == true) { 
+    //       console.log(returned_value["medical-history"])
+    //       sessionStorage.setItem("labHistory", returned_value["lab-history"])
+    //       sessionStorage.setItem("medicalHistory", returned_value["medical-history"])
+    //       window.location.href = "main/"
+    //     }
+    //   },
+    //   error: function () { }
+    // });
+  })
