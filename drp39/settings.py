4458fe8200970dcc59910b3efe73863ba2761ce7
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-01 14:52:41 +0100

    Fix collectstatic again

--- drp39/settings.py
+++ drp39/settings.py
@@ -34,6 +34,9 @@ ALLOWED_HOSTS = [WEBAPP_HOST, LOCAL_HOST]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 # Application definition
 
@@ -185,9 +188,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
