b9f3a23f861e61cd2425e0826e06b7d1872f027a
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-03 23:08:39 +0100

    Change DEBUG back to False for stable deployment test

--- drp39/settings.py
+++ drp39/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = False
 
 LOCAL_HOST = '127.0.0.1'
 WEBAPP_HOST = 'patientoncall.herokuapp.com'
