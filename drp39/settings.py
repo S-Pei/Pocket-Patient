a7e0f4cb11a7d5e794d8d9b4316cd6a766ef07ee
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-03 14:19:49 +0100

    Implement DEBUG field check to .env

--- drp39/settings.py
+++ drp39/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = str(os.getenv('DEBUG')) if str(os.getenv('DEBUG')) else False
 
 LOCAL_HOST = '127.0.0.1'
 WEBAPP_HOST = 'patientoncall.herokuapp.com'
