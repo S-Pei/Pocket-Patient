292540bb31dcbc1e729461f0513c2dc2ec260df9
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-05 17:08:46 +0100

    Create wait for patient confirmation UI

--- patientOnCall/static/patientOnCall/index.js
+++ patientOnCall/static/patientOnCall/index.js
@@ -13,20 +13,27 @@ document.getElementById("patient-search-submit").addEventListener("click", (e) =
         'patientName': patientName
     },
     success: function (returned_value) {
-      if (returned_value.ok == true) { 
-        // console.log(returned_value["patient-first-name"])
-        sessionStorage.setItem("sessionID", returned_value["sessionId"])
-        sessionStorage.setItem("patientID", patientId)
-        sessionStorage.setItem("patientFirstName", returned_value["patient-first-name"])
-        sessionStorage.setItem("patientLastName", returned_value["patient-last-name"])
-        sessionStorage.setItem("patientDob", returned_value["patient-dob"])
-        sessionStorage.setItem("patientAddress", returned_value["patient-address"])
-        sessionStorage.setItem("labHistory", JSON.stringify(returned_value["lab-history"]))
-        sessionStorage.setItem("medicalHistory", JSON.stringify(returned_value["medical-history"]))
-        sessionStorage.setItem("prescription", JSON.stringify(returned_value["prescription"]))
-        window.location.href = "main/"
+      if (returned_value.ok == true) {
+        // Wait for patient approval
+        wait_for_patient_approval();
+
+        // sessionStorage.setItem("sessionID", returned_value["sessionId"])
+        // sessionStorage.setItem("patientID", patientId)
+        // sessionStorage.setItem("patientFirstName", returned_value["patient-first-name"])
+        // sessionStorage.setItem("patientLastName", returned_value["patient-last-name"])
+        // sessionStorage.setItem("patientDob", returned_value["patient-dob"])
+        // sessionStorage.setItem("patientAddress", returned_value["patient-address"])
+        // sessionStorage.setItem("labHistory", JSON.stringify(returned_value["lab-history"]))
+        // sessionStorage.setItem("medicalHistory", JSON.stringify(returned_value["medical-history"]))
+        // sessionStorage.setItem("prescription", JSON.stringify(returned_value["prescription"]))
+        // window.location.href = "main/"
       }
     },
     error: function () { }
   });
 })
+
+function wait_for_patient_approval() {
+  $("#patient-search-form").addClass("invisible");
+  $("#waiting-for-confirmation-box").removeClass("invisible");
+}
