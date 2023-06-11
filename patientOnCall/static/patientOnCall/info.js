4b1e9fb609e90b9f986637837157c682e3624655
Yin <yxl21@ic.ac.uk>
2023-06-11 16:11:07 +0100

    add hover and click to pastMedHistory

--- patientOnCall/static/patientOnCall/info.js
+++ patientOnCall/static/patientOnCall/info.js
@@ -19,13 +19,17 @@ var base_url = window.location.origin;
     //   = "Address: " + address
 
     insertMedHistoryEntries(medicalHistory);
+    for(var i = 1; i <= medicalHistory.length; i ++) {
+        row_hover(i, medicalHistory[i-1]["visitType"]);
+        row_click(i);
+    }
 })();
 
 function insertMedHistoryEntries(medicalHistory) {
   var i = 0
   while (i < medicalHistory.length) {
     console.log(medicalHistory[i])
-      addMedHistoryEntry(
+      addMedHistoryEntry(i+1, 
       medicalHistory[i]["dischargeDate"],
        medicalHistory[i]["summary"],
        medicalHistory[i]["addToMedicalHistory"])
@@ -33,14 +37,15 @@ function insertMedHistoryEntries(medicalHistory) {
   }
 }
 
-function addMedHistoryEntry(dischargeDate, summary, addToMedicalHistory) {
+function addMedHistoryEntry(rowNum, dischargeDate, summary, addToMedicalHistory) {
     // Create a new entry for the table
     var tableBody = document.getElementById("past-medical-history-entries");
-    if (addToMedicalHistory === true){
+    var row = "row-" + rowNum
+    if (addToMedicalHistory === true && summary !== ""){
         const newEntry = document.createElement("li");
         newEntry.classList.add("past-medical-history-entry", "p-1", "d-flex", 
             "flex-row", "w-100", "mb-2", "rounded-3", "border", "text-black-50")
-    
+        newEntry.classList.add(row)
         const entryDischargeDate = document.createElement("div");
         entryDischargeDate.classList.add("past-medical-history-discharge-date");
         entryDischargeDate.textContent = dischargeDate;
@@ -55,5 +60,35 @@ function addMedHistoryEntry(dischargeDate, summary, addToMedicalHistory) {
         tableBody.appendChild(newEntry);
 
         console.log(tableBody);
+    }
+}
+
+function row_hover(rowNum, visitType){
+    var rowClass = 'row-' + rowNum 
+    var row = document.getElementsByClassName(rowClass);
+    var n = row.length;
+    function changeColor(color){
+        for(var i = 0; i < n; i++) {
+            row[i].style.backgroundColor = color; 
+        }
+    }
+    for(var i = 0; i < n; i ++) {
+        row[i].onmouseover = function() {
+            changeColor("#A1DADE");
+        };
+        row[i].onmouseout = function() {
+            changeColor("");
+        };   
+    }
+}
+
+function row_click(rowNum){
+    var rowClass = 'row-' + rowNum 
+    var row = document.getElementsByClassName(rowClass);
+    var n = row.length;
+    for(var i = 0; i < n; i ++) {
+        row[i].onclick = function() {
+            window.location.href = base_url + "/edit-visit/" + rowNum
+        };
     }
 }
