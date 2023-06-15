2c5bbe2b0b5da8aa55bbd566f8bcc4290a4da9e5
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-15 23:16:28 +0100

    Create initial UI for diary categories

--- patientOnCall/static/patientOnCall/patient-diary-categories.js
+++ patientOnCall/static/patientOnCall/patient-diary-categories.js
@@ -7,5 +7,27 @@
   document.getElementById("patient-name").innerHTML = firstName + ' ' + lastName
   document.getElementById("patient-id").innerHTML = 'NHS Number:' + patientID
 
-  // getDiaryData();
+  insertDiaryCategories();
 })();
+
+function insertDiaryCategories() {
+  let diaryData = JSON.parse(sessionStorage.getItem("patientDiary"));
+  for (category of Object.keys(diaryData)) {
+    let categoryElement = createCategoryElement(category);
+    insertCategoryElement(categoryElement);
+  }
+}
+
+function createCategoryElement(category) {
+  let categoryElementBox = document.createElement("div");
+  categoryElementBox.classList.add("category-item-box");
+  let categoryElement = document.createElement("div");
+  categoryElement.classList.add("category-item");
+  categoryElement.innerText = category;
+  categoryElementBox.appendChild(categoryElement);
+  return categoryElementBox;
+}
+
+function insertCategoryElement(categoryElement) {
+  $("#categories-grid-box").append(categoryElement);
+}
