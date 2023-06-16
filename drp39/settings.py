c030b639a1a3c05e730d46225e7f34b089ea29a8
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-16 14:32:46 +0100

    Set db conn_max_age to 0

--- drp39/settings.py
+++ drp39/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
 
 if not DEBUG:
     import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=600)
+    db_from_env = dj_database_url.config(conn_max_age=0)
     DATABASES['default'].update(db_from_env)
 
 
