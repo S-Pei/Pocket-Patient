f97872a63a08b9a060474193865ff582ddf11a54
hsh121 <huan.ham21@imperial.ac.uk>
2023-06-20 00:49:21 +0100

    Increase access token time

--- drp39/settings.py
+++ drp39/settings.py
@@ -138,7 +138,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=8),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
